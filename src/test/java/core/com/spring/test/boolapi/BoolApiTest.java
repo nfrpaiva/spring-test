42ab1ed74995e9749563e1b063015fa9d3af5379
Nilton Fernando <nfrpaiva@gmail.com>
2016-05-27 08:43:01 -0300

    Removidos os templates de licença do netbeans

--- src/test/java/core/com/spring/test/boolapi/BoolApiTest.java
+++ src/test/java/core/com/spring/test/boolapi/BoolApiTest.java
@@ -1,42 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package core.com.spring.test.boolapi;
 
-import static com.lordofthejars.bool.Bool.*;
-import org.hamcrest.MatcherAssert;
+import static com.lordofthejars.bool.Bool.the;
+
 import static org.hamcrest.Matchers.*;
+import org.hamcrest.MatcherAssert;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
- *
  * @author fernando
  */
 public class BoolApiTest {
 
-    @Test
-    public void test() {
-        boolean isValid = the("A", is(lessThan("B")));
-        boolean isValid2 = is(lessThan("B")).matches("A");
-        boolean isValid3 = false;
+	@Test
+	public void test() {
+		boolean isValid = the("A",lessThan("B"));
+		boolean isValid2 = lessThan("B").matches("A");
+		boolean isValid3 = false;
 
-        if ("A".compareTo("B") == -1) {
-            isValid3 = true;
-        }
+		if ("A".compareTo("B") == -1) {
+			isValid3 = true;
+		}
 
-        Assert.assertTrue(isValid);
-        Assert.assertTrue(isValid2);
-        Assert.assertTrue(isValid3);
-    }
+		Assert.assertTrue(isValid);
+		Assert.assertTrue(isValid2);
+		Assert.assertTrue(isValid3);
+	}
 
-    @Test
-    public void test2() {
-        String value = "teste";
-        MatcherAssert.assertThat(2, either(is(1)).or(is(2)));
-        MatcherAssert.assertThat("nome", isA(String.class));
-        MatcherAssert.assertThat("nao deu", value, anyOf(nullValue(), equalTo("teste")));
-    }
+	@Test
+	public void test2() {
+		String value = "teste";
+		MatcherAssert.assertThat(2,either(is(1)).or(is(2)));
+		MatcherAssert.assertThat("nome",isA(String.class));
+		MatcherAssert.assertThat("nao deu",value,anyOf(nullValue(String.class),equalTo("teste")));
+	}
 }
